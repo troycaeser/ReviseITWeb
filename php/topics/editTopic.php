46db29d53d95724f44af8c82ebc9082d3a21f221
Kris Vega <krisv_990@hotmail.com>
2013-03-25 16:36:13 +1100

    Fixed the format

--- php/topics/editTopic.php
+++ php/topics/editTopic.php
@@ -4,7 +4,6 @@
 
 	$topic_ID = $_GET['ID'];
 
-	//$path_parts['filename'] echoes out the last part of the url.
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
