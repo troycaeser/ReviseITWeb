b6e108b66206ae2b79694d411a520c96c4ae8e21
Kris Vega <krisv_990@hotmail.com>
2013-04-15 15:13:30 +1000

    Added menu bar for student home page

--- php/student/studentHome.php
+++ php/student/studentHome.php
@@ -4,25 +4,14 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width-device-width, initial-scale=1.0">
 <title>ReviseIT - Student</title>
-<link rel="stylesheet" href="../../assets/css/version1.css">
-<link rel="stylesheet" href="../../assets/css/bootstrap-responsive.css">
+		<?php
+			include '../header_container.php';
+		?>
 </head>
 <body>
-<div class="navbar navbar-fixed-top navbar-inverse">
-  <div class="navbar-inner">
-    <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-th-list"></span> </a> <a href="#" class="brand">reviseIT</a>
-      <p class="nav navbar-text">user type: <strong>student</strong></p>
-      <div class="nav-collapse collapse">
-        <ul class="nav pull-right">
-          <li class="active"><a href="studentHome.php">Home</a></li>
-          <li><a href="../subjects/all_Subjects.php">Subjects</a></li>
-          <li><a href="#">Enrol</a></li>
-          <li><a href="#">Account</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</div>
+<?php
+	include 'student_menu_bar.php';
+?>
 <br />
 <br />
 <div class="container">
@@ -38,7 +27,7 @@
         <div class="span3">
           <h3>Create an Account!</h3>
           <p>Enter a token and register an account!</p>
-          <a href="../EnterToken.php">Register</a> </div>
+          <a href="../EnterToken.php" class="btn btn-primary" rel="popover" data-original-title="Register">Register</a> </div>
         <div class="span3">
           <h3>Enrol in Subject</h3>
           <p>Obtain access to a subject and it's content!</p>
@@ -46,7 +35,7 @@
         <div class="span3">
           <h3>View Content</h3>
           <p>Access Revision Content and Tests!</p>
-          <a>Enrol</a> </div>
+          <a>View Content</a> </div>
         <div class="span3">
           <h3>View Test Results</h3>
           <p>View Previous Test Results!</p>
@@ -56,25 +45,28 @@
     <div class="span4">
       <ul class="nav nav-list">
         <li class="nav-header">Quick Access</li>
-        <li class="active"><a href="#">Help</a></li>
+        <li class="active"><a href="#">My Account</a>
         <li><a href="#">Contact Admin</a></li>
-        <li><a href="#">My Account</a></li>
+        </li><a href="#">Help</a></li>
       </ul>
     </div>
   </div>
 </div>
-<div class="navbar navbar-fixed-bottom">
-  <div class="container">
-    <div class="nav-collapse collapse">
-      <ul class="nav pull-right">
-        <li><a href="#">Log out</a></li>
-        <li><a href="#">Contact Admin</a></li>
-        <li><a href="#">Help</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
+<?php
+	include '../footer.php';
+?>
 <script src="http://code.jquery.com/jquery-1.9.0.min.js"></script> 
 <script src="../../assets/js/bootstrap.js"></script>
+<script src="../../assets/js/bootstro.min.js"></script>
+		<script>
+		$(document).ready(function(){
+
+		
+			$('bootstroTest').click(function(){
+				bootstro.start(".bootstro");
+				//$('#example').popover({trigger: "hover"});
+			});
+		});
+		</script>
 </body>
 </html>
