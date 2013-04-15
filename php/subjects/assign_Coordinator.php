78246c3d9e597368ba3a87e186e2bbb30cc1fdb4
Kris Vega <krisv_990@hotmail.com>
2013-04-15 17:18:03 +1000

    Created assign coordinator page

---
+++ php/subjects/assign_Coordinator.php
@@ -0,0 +1,30 @@
+<?php
+  include '../getConnection.php';
+  require '../check_logged_in.php';
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php
+	include '../header_container.php';
+?>
+<title>reviseIT - Subject Coordinator</title>
+</head>
+
+<body>
+<br /><br />
+<div class="container">
+	<div class="page-header">
+    	<h1>Assign Subject Coordinator</h1>
+    </div>
+    
+    <div class="row-fluid">
+    </div>
+</div>
+
+<?php
+	include '../footer.php';
+?>
+</body>
+</html>
