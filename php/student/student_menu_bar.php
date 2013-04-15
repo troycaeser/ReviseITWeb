6bf86b326d5f94eeefdb4d4ccc5706fb0a7a3e10
Kris Vega <krisv_990@hotmail.com>
2013-04-15 15:14:19 +1000

    Created menu bar for student

---
+++ php/student/student_menu_bar.php
@@ -0,0 +1,15 @@
+<div class="navbar navbar-fixed-top navbar-inverse">
+  <div class="navbar-inner">
+    <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-th-list"></span> </a> <a href="#" class="brand">reviseIT</a>
+      <p class="nav navbar-text">user type: <strong>student</strong></p>
+      <div class="nav-collapse collapse">
+        <ul class="nav pull-right">
+          <li class="active"><a href="studentHome.php">Home</a></li>
+          <li><a href="../subjects/all_Subjects.php">Subjects</a></li>
+          <li><a href="#">Enrol</a></li>
+          <li><a href="#">Account</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
