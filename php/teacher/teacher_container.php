eafd7f85a726fc372ab8bcefb5f23641385c1fbd
Kris Vega <krisv_990@hotmail.com>
2013-04-15 15:29:59 +1000

    All Subjects href uses bootstro

--- php/teacher/teacher_container.php
+++ php/teacher/teacher_container.php
@@ -8,6 +8,6 @@
 	<div class="span6">
 		<h3>See All Subjects!</h3>
 		<p>See your subjects and upload some contents!</p>
-		<a href="../subjects/all_Subjects.php">All Subjects</a>
+		<a href="../subjects/all_Subjects.php" class="btn btn-primary" rel="popover" data-original-title="All Subjects">All Subjects</a>
 	</div>
 </div>
