f05778d3a3d5b4dcb8724c169fdd07c9c5f9328c
Kris Vega <krisv_990@hotmail.com>
2013-04-15 17:15:35 +1000

    Added PDO functionality

--- php/subjects/delete_subject.php
+++ php/subjects/delete_subject.php
@@ -1,24 +1,9 @@
 <?php
 
-	include '../init.php';
-
-	//to create subject you need SubjectID, SubjectCode(3char 5 num), SubjectName, UserID, Dateupdated
-
-	//this query selects teachers & coordinators, and displays only user id and names.
-	$query_teachers = mysql_query("SELECT UserID, fName, lName FROM users WHERE role = 2 OR role = 3");
-
-	//assign variables.
-	$subject_code = $_POST['subject_code'];
-	$subject_name = $_POST['subject_name'];
-	$selected_teacher = $_POST['subject_coordinator'];
-
-	//get the date in Australia and assign variable
-	date_default_timezone_set('Australia/Melbourne');
-	$date = date('Y-m-d', time());
-
-	//insert subject query.
-	$query_subject = "INSERT INTO subject VALUES(null,'".$subject_code."','".$subject_name."', '".$selected_teacher."', '".$date."')";
-
+	include '../getConnection.php';
+	require '../check_logged_in.php';
+	
+	$subject_ID = $_GET['ID'];
 ?>
 
 <html>
@@ -31,7 +16,6 @@
 	</head>
 	
 	<body>
-
 		<div class="navbar navbar-fixed-top navbar-inverse">
 			<div class="navbar-inner">
 				<div class="container">
@@ -44,7 +28,7 @@
 						<ul class="nav pull-right">
 							<li class="active"><a href="#">Home</a></li>
 							<li><a href="#">Accounts</a></li>
-							<li><a href="#">Subjects</a></li>
+							<li><a href="all_Subjects.php">Subjects</a></li>
 						</ul>
 					</div>
 				</div>
@@ -54,51 +38,31 @@
 		<br /><br />
 
 		<div class="container">
-
-			<div class="page-header">
-				<h1>Add A Subject</h1>
-			</div>
-
-			<div class="row_fluid">
-				<div class="span7">
-					<form class="form-horizontal" method="post" action='<?php echo($_SERVER["PHP_SELF"]); ?>'>
-						<fieldset>
-							<div class="control-group">
-								<label class="control-label" for="subject_code">Subject Code</label>
-								<div class="controls">
-									<input type="text" name="subject_code" id="subject_code" value="" />
-								</div>
-							</div>
-
-							<div class="control-group">
-								<label class="control-label" for="subject_name">Subject Name</label>
-								<div class="controls">
-									<input type="text" name="subject_name" id="subject_name" value="" />
-								</div>
-							</div>
-
-							<div class="control-group">
-								<label class="control-label" for="subject_coordinator">Coordinator</label>
-								<div class="controls">
-									<?php
-										//echo out the select table (this is for the user id.)
-									    echo "<select name='subject_coordinator' id='subject_coordinator' multiple='multiple'>";
-										    while($row = mysql_fetch_array($query_teachers)){
-												echo("<option value = '" . $row['UserID'] . "'>". $row['fName'] ." ". $row['lName'] ."</option>");
-											}
-										echo "</select>";
-									?>
-								</div>
-							</div>
-
-							<div class="controls">
-								<button class="btn" type="submit" name="reset">Reset</button>
-								<button class="btn btn-primary" type="submit" name="submit">Submit</button>
-							</div>
-						</fieldset>
-					</form>
-				</div>
-
+        <?php	
+		
+		$topic_ID = $_GET['id'];
+        if (isset($_GET['id']) && is_numeric($_GET['id']))
+	 	{
+			try
+			{
+				// Delete the entry
+				$result = $db->prepare("DELETE FROM subject WHERE SubjectID='".$topic_ID."'");
+				$result->execute();
+			}
+			catch(PDOExcepiton $e)
+			{
+				echo $e->getMessage();
+			}
+				
+			 // Redirect back to the view page
+			 header("Location: all_subjects.php");
+		}
+		else
+		// If id isn't set, or isn't valid, redirect back to the view page
+		{
+			header("Location: all_subjects.php");
+		}
+	 ?>
 				<div class="span4">
 					<ul class="nav nav-list">
 						<li class="nav-header">Quick Access</li>
@@ -121,11 +85,3 @@
 		<script src="../../js/bootstrap.js"></script>
 	</body>
 </html>
-
-<?php
-	if (isset($_POST["submit"])){
-		mysql_query($query_subject) or die(mysql_error());
-
-		echo "1 record added";
-	}
-?>
