6c9012d3128138d7b3f0461c68fdc0089343cf1b
Kris Vega <krisv_990@hotmail.com>
2013-04-22 02:22:16 +1000

    Fixed a format issue

--- php/subjects/subjects.php
+++ php/subjects/subjects.php
@@ -36,7 +36,7 @@
 					echo "<div class='span2'><h4>Coordinator</h4></div>";
 				echo "</div>";
 	
-				echo "<form action='edit_subject.php' method='post'>";
+				echo "<form action='' method='post'>";
 					//display everything in a row-fluid/spans while looping the result.
 					//pass SubjectID in the url for each individual link.
 					while($row = $result->fetch(PDO::FETCH_ASSOC))
@@ -67,10 +67,11 @@
 			echo "<div class='span8 bootstro' data-bootstro-placement='bottom' data-bootstro-title='List of Subjects' data-bootstro-content='You may click on one of these links to go to the topics page that is associated with the selected subject.'>";
 			echo "<div class='row-fluid'>";
 				echo "<div class='span1'></div>";
-				echo "<div class='span5'><h4>Subject</h4></div>";
+				echo "<div class='span3'><h4>Subject</h4></div>";
 				echo "<div class='span2'><h4>Code</h4></div>";
 				echo "<div class='span2'><h4>Date</h4></div>";
 				echo "<div class='span2'><h4>Coordinator</h4></div>";
+				echo "<div class='span2'></div>";
 			echo "</div>";
 
 			echo "<form action='edit_subject.php' method='post'>";
@@ -78,20 +79,24 @@
 				//pass SubjectID in the url for each individual link.
 				while($row = $result->fetch(PDO::FETCH_ASSOC))
 				{
-					//adds checklist for each item.
-					echo "<div class='span1'>";
-						echo "<label class='checkbox'>";
-							echo "<input name='chk_group[]' value='".$row['SubjectID']."' type='checkbox' />";
-						echo "</label>";
-					echo "</div>";
+					
 
 					//display subjects in a list style with anchor pointing to the subject's topics
 					echo "<a href='../topics/viewTopic.php?ID=".$row['SubjectID']."'>";
 						echo "<div name='subject_ID".$row['SubjectID']."' id='".$row['SubjectID']."' class='row-fluid'>";
-							echo "<div class='span5'>".$row['SubjectName']."</div>";
+						
+						//adds checklist for each item.
+							echo "<div class='span1'>";
+								echo "<label class='checkbox'>";
+									echo "<input name='chk_group[]' value='".$row['SubjectID']."' type='checkbox' />";
+								echo "</label>";
+							echo "</div>";
+							
+							echo "<div class='span3'>".$row['SubjectName']."</div>";
 							echo "<div class='span2'>".$row['SubjectCode']."</div>";
 							echo "<div class='span2'>".$row['Dateupdated']."</div>";
 							echo "<div class='span2'>".$row['fName']." ".$row['lName']."</div>";
+							echo "<div class='span2'><a href='delete_subject.php?ID=".$row['SubjectID']."'>".Delete."</a></div>";	
 						echo "</div>";
 					echo "</a>";
 				}
@@ -99,12 +104,6 @@
 			echo "</form>";
 		echo "</div>";
 		}
-
-
-		
-		//echo '<pre>', print_r($result, true), '</pre>';
-		//echo '<pre>', print_r($stuff, true), '</pre>';
-
 	}
 	catch(PDOException $e)
 	{
