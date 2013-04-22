dfe5441172ca08fbeaf8c2e1f399d5cc95cd50d2
Kris Vega <krisv_990@hotmail.com>
2013-04-23 01:32:33 +1000

    Got 3 attempts working, message displays, currently working on blocking account

--- php/login.php
+++ php/login.php
@@ -1,9 +1,7 @@
 <?php
-	
-	try{
-
+	try
+	{
 		include 'getConnection.php';
-		//$db = dbConnect();
 
 			$username = $_POST['username'];
 			$password = $_POST['password'];
@@ -35,80 +33,42 @@
 
 					header("Location: home_page_director.php");
 
-	 			    //echo '<pre>', print_r($role, true), '</pre>';
-
-	 			    //direct user to home page (depending on the role.)
-					//include 'Parameters.php';
-					//provideAccess($role);
-				}else{
-					echo "incorrect username or password";
 				}
-
-			}else{
+				else
+				{
+					if(isset($_SESSION['login_attempt']))
+					{
+						if($_SESSION['login_attempt'] < 3)
+						{
+							$attempt = $_SESSION['login_attempt'] + 1;
+							
+							if($attempt = 3)
+							{
+								echo "<script type='text/javascript'>alert('Cannot log in')</script>";
+							}
+						}
+						else
+						{
+							echo "<script type='text/javascript'>alert('No log')</script>";
+						}
+					}
+					else
+					{
+						header("Location: index.php");
+					}
+				}
+			}
+			else
+			{
 				echo "please enter username and password";
 			}
 
 		//login($username, $password);
 
-	}catch(PDOException $e){
+	}
+	catch(PDOException $e)
+	{
 		echo $e->getMessage();
 	}
-	
-
-	//$db = getConnection();
-
-	/*if(empty($_POST) === false){
-
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-
-		if(empty($username) == true || empty($password) == true){
-			$errors[] = 'You need to enter a username and password';
-		}
-		else if(user_exists($username) === false){
-			$errors[] = 'Incorrect username!';
-		}
-		else{
-			$query = mysql_query("SELECT COUNT(`UserID`) FROM `users` WHERE `username` = '$username' AND `password` = '$password'");
-			echo mysql_result($query, 0);
-			//$password = md5($password);
-			
-			$login = login($username, $password);
-			if($login === false){
-				$errors[] = 'username and password do not match.';
-			}
-			else{
-				//set the user session as the user ID, it is unique. $login returns user ID.
-				$_SESSION['UserID'] = $login;
-				
-				//gets the role of the username
-				include 'role.php';
- 			    $role = user_type_from_username($username);
-
-				//direct user to home page (depending on the role.)
-				include 'Parameters.php';
-				provideAccess($role);
-			}
-		}
-		//print_r($errors);
-
-	}
-
-	function user_exists($username){
-		$query = mysql_query("SELECT COUNT(`UserID`) FROM `users` WHERE `username` = '$username'");
-		return (mysql_result($query, 0) == 1) ? true : false;
-	}
-
-	function user_id_from_username($username){
-		$query = mysql_query("SELECT `UserID` FROM `users` WHERE `username` = '$username'");
-		return mysql_result($query, 0, 'UserID');
-	}
-
-	function login($username, $password){
-		$user_id = user_id_from_username($username);
-
-		$query = mysql_query("SELECT COUNT(`UserID`) FROM `users` WHERE `username` = '$username' AND `password` = '$password'");
-		return (mysql_result($query, 0) == 1) ? $user_id : false;
-	}
-*/
 ?>
+
