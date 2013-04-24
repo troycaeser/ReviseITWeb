25fa5371c2c1dc8d460495e738cda7eac8fc61d3
Kris Vega <krisv_990@hotmail.com>
2013-04-25 03:32:56 +1000

    Stored User Type for session to display username

--- php/admin/admin_menu_bar.php
+++ php/admin/admin_menu_bar.php
@@ -5,7 +5,7 @@
 				<span class="icon-th-list"></span>
 			</a>
 			<a href="../home_page_director.php" class="brand">reviseIT</a>
-			<p class="nav navbar-text">user type: <strong>administrator</strong></p>
+			<p class="nav navbar-text"><?php session_start(); $userID = $_SESSION['UserID']; echo "Welcome, ".$userID;?></strong></p>
 			<div class="nav-collapse collapse">
 				<ul class="nav pull-right">
 					<li class="active bootstro" data-bootstro-step="4" data-bootstro-placement="bottom" data-bootstro-title="Home Page" data-bootstro-content="Click this link to go to the <b>Home</b> page."><a href="admin_Home.php">Home</a></li>
