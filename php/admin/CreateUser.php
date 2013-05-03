78cae9501d87a3b873be17c4676a6f1a78b3b65d
MAR08274004 <d.marsh71@live.com.au>
2013-05-03 17:23:38 +1000

    Create User fixed + DAL

--- php/admin/CreateUser.php
+++ php/admin/CreateUser.php
@@ -1,100 +1,145 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-
-<body>
 <?php
+  include '../getConnection.php';
+  require '../check_logged_in.php';
+?>
 
-function connectDB() {
-	try{
-		$hostname = 'localhost';
-		$username = 'root';
-		$password = 'root';
-		$dbname = 'reviseit';
-		$dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
-		return $dbh;
-	}
-	catch (PDOException $e){
-		if($dbh != null) $dbh = null;
-		die("Could not connect to database: - ".$e->getMessage());
-	}
-}
+<!DOCTYPE html>
+<html>
+<head>
+<?php 
+  require_once("../../DAL/Verification.php"); 
+  require_once("../../DAL/DataAccessLayer.php");
+  include '../header_container.php';
+?>
+</head>
+<body>
+</head>
+<body>
+  <?php
+    include 'admin_menu_bar.php';
+  ?>
+<br /><br />
+<div class="container">
+  <div class="page-header">
+    <h1>Revise IT - Create New User Account</h1>
+  </div>
+  <div class='row-fluid'>
+    <div class='span8'>
+      <?php
+$setfName = 0;
+$setlName = 0;
+$setuserName = 0;
 
-function enterToken($token){
-	try{
-		$dbh = connectDB();
-		$sql = "SELECT tokenCode from token ORDER BY tokenDate DESC LIMIT 1;"; 
-			$stmt=$dbh->prepare($sql);
-			$stmt->bindParam("tokenCode", $token);
-			$stmt->execute();
-			$row=$stmt->fetch(PDO::FETCH_OBJ);
-			$dbh = null;
-			if ($row->tokenCode == $token) return true; 
-			else return false;
-	}
-	catch (PDOException $e){
-		if($dbh != null) $dbh = null;
-		echo("Could not verify token: Error: - ".$e->getMessage());
-		return false;
-	}
-}
+$fName = "";
+$lName = "";
+$userName = "";
 
-function createToken($token){
-	try{
-		$dbh = connectDB();
-		date_default_timezone_set('Australia/Melbourne');
-		$datetime = date('Y/m/d H:i:s');
-		$sql = "INSERT INTO token (tokenCode, tokenDate) VALUES ('".$token."', '".$datetime."');"; 
-			$stmt=$dbh->prepare($sql);
-			$stmt->bindParam("tokenCode", $token);
-			$stmt->bindParam("tokenDate", $datetime);
-			$stmt->execute();
-			$dbh = null;
-			return true;
-	}
-	catch (PDOException $e){
-		if($dbh != null) $dbh = null;
-		echo("Could not create token: Error: - ".$e->getMessage());
-		return false;
-	}
-}
-
-function createUser($username, $password, $fName, $lName, $role){
-	try{
-		$dbh = connectDB();
-		$sql = "SELECT * FROM users WHERE username = '".$username."';"; 
-		$stmt=$dbh->prepare($sql);
-		$stmt->bindParam("username", $username);
-		$stmt->execute();
-		if ($row != NULL) return "error";		
-	}
-	catch (PDOException $e){
-		if($dbh != null) $dbh = null;
-		echo("Could not create Account: Error: - ".$e->getMessage());
-		return false;
-	}
-	
-	try {
-		$sql = "INSERT INTO users (username, password, fName, lName, role) VALUES ('".$username."', '".$password."', '".$fName."', '".$lName."', '".$role."');"; 
-		$stmt=$dbh->prepare($sql);
-		$stmt->bindParam("username", $username);
-		$stmt->bindParam("password", $password);
-		$stmt->bindParam("fName", $fNname);
-		$stmt->bindParam("lName", $lName);
-		$stmt->bindParam("role", $role);
-		$stmt->execute();
-		$dbh = null;
-		return true;
-	}
-	catch (PDOException $e){
-		if($dbh != null) $dbh = null;
-		echo("Could not create Account: Error: - ".$e->getMessage());
-		return false;
+if(isset($_POST["submitUser"]))
+{
+		if ($_POST["fName"] == NULL)
+			$setfName = 1;
+		else $fName = $_POST["fName"];
+		if ($_POST["lName"] == NULL)
+			$setlName = 1;
+		else $lName = $_POST["lName"];
+		if ($_POST["userName"] == NULL)
+			$setuserName = 1;
+		else $userName = $_POST["userName"];
+		$pass1 = $_POST["pass1"];
+		$pass2 = $_POST["pass2"];
+		
+		if ($pass1 != $pass2)
+			echo ("<p class='errmsg'>Passwords do not match!</p>");
+		elseif (!verifyPassword($pass1))
+			echo ("<p class='errmsg'>Password requires Capital, Small, Numeral and at least eight characters, No Special Characters!</p>");
+		else { $pass = $pass1;
+			$error = "";
+			$error = createUser($userName, $pass, $fName, $lName, "3");
+			echo $error;
+			if($error === "error") echo "<p class='errmsg'>Username already exists</p>";
+			else header('Location: all_Accounts.php');
 	}
 }
 ?>
+      <form class="form-horizontal" method="post" action='<?php echo($_SERVER["PHP_SELF"]); ?>'>
+        <div class="center">
+          <fieldset>
+            <div class="control-group">
+              <label class="control-label" for="fName">Enter First Name</label>
+              <div class="controls">
+                <input type="text" name="fName" id="fName" value='<?php echo $fName ?>'/>
+              </div>
+            </div>
+            <?php if ($setfName) echo "<tr><td colspan='2' class='errmsg'>Please enter a first name!</td></tr>"; ?>
+            <div class="control-group">
+              <label class="control-label" for="lName">Enter Last Name</label>
+              <div class="controls">
+                <input type="text" name="lName" id="lName" value='<?php echo $lName ?>' />
+              </div>
+            </div>
+            <?php if ($setlName) echo "<tr><td colspan='2' class='errmsg'>Please enter a last name!</td></tr>"; ?>
+            <div class="control-group">
+              <label class="control-label" for="userName">Enter Username</label>
+              <div class="controls">
+                <input type="text" name="userName" id="userName" value='<?php echo $userName ?>' />
+              </div>
+            </div>
+            <?php if ($setuserName) echo "<tr><td colspan='2' class='errmsg'>Please enter a username!</td></tr>"; ?>
+            <div class="control-group">
+              <label class="control-label" for="pass1">Enter Password</label>
+              <div class="controls">
+                <input type="password" name="pass1" id="pass1" value='' />
+              </div>
+            </div>
+            <div class="control-group">
+              <label class="control-label" for="pass2">Confirm Password</label>
+              <div class="controls">
+                <input type="password" name="pass2" id="pass2" value='' />
+              </div>
+            </div>
+            <div class="controls">
+              <input class="btn" type="submit" name="submitUser" value="SUBMIT" />
+              <input class="btn" type="submit" name="reset" value="RESET" />
+            </div>
+          </fieldset>
+        </div>
+      </form>
+    </div>
+    <?php if (isset($POST["reset"]))
+	{
+		$setfName = 0;
+		$setlName = 0;
+		$setuserName = 0;
+	
+		$fName = "";
+		$lName = "";
+		$userName = "";
+		$pass1 = "";
+		$pass2 = "";
+	}
+?>
+  <div class="span4">
+    <ul class="nav nav-list">
+      <li class="nav-header">Quick Access</li>
+      <li class="active"><a href="#">Help</a></li>
+      <li><a href="#">Contact Admin</a></li>
+      <li><a href="#">My Account</a></li>
+    </ul>
+  </div>
+</div>
+</div>
+<div class="navbar navbar-fixed-bottom">
+  <div class="container">
+    <div class="nav-collapse collapse">
+      <ul class="nav pull-right">
+        <li><a href="../logout.php">Log out</a></li>
+        <li><a href="#">Contact Admin</a></li>
+        <li><a href="#">Help</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+<script src="http://code.jquery.com/jquery-1.9.0.min.js"></script> 
+<script src="../../assets/js/bootstrap.js"></script>
 </body>
 </html>
