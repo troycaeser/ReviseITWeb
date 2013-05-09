b03f1b12348e9540c9f3f2cef6bdc5a6426d5758
troycaeser <troycaeser@hotmail.com>
2013-05-09 21:33:04 +1000

    fixed headers error on webservices, removed redundant files on subjects

--- php/subjects/delete_subject.php
+++ php/subjects/delete_subject.php
@@ -4,76 +4,19 @@
 	require '../check_logged_in.php';
 	
 	$subject_ID = $_GET['ID'];
+		
+	$subjectID = $_GET['DELETEID'];
+	try
+	{
+		// Delete the entry
+		$result = $db->prepare("DELETE FROM subject WHERE SubjectID = :bind_SubjectID");
+		$result->bindParam("bind_SubjectID", $subjectID);
+		$result->execute();
+
+	 	header("Location: all_subjects.php");
+	}
+	catch(PDOExcepiton $e)
+	{
+		echo $e->getMessage();
+	}
 ?>
-
-<html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width-device-width, initial-scale=1.0">
-		<title>ReviseIT - Teacher</title>
-		<link rel="stylesheet" href="../../assets/css/version1.css">
-		<link rel="stylesheet" href="../../assets/css/bootstrap-responsive.css">
-	</head>
-	
-	<body>
-		<div class="navbar navbar-fixed-top navbar-inverse">
-			<div class="navbar-inner">
-				<div class="container">
-					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-						<span class="icon-th-list"></span>
-					</a>
-					<a href="#" class="brand">reviseIT</a>
-					<p class="nav navbar-text">user type: <strong>teacher</strong></p>
-					<div class="nav-collapse collapse">
-						<ul class="nav pull-right">
-							<li class="active"><a href="#">Home</a></li>
-							<li><a href="#">Accounts</a></li>
-							<li><a href="all_Subjects.php">Subjects</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<br /><br />
-
-		<div class="container">
-        <?php	
-		
-		$subjectID = $_GET['DELETEID'];
-		try
-		{
-			// Delete the entry
-			$result = $db->prepare("DELETE FROM subject WHERE SubjectID = :bind_SubjectID");
-			$result->bindParam("bind_SubjectID", $subjectID);
-			$result->execute();
-
-		 	header("Location: all_subjects.php");
-		}
-		catch(PDOExcepiton $e)
-		{
-			echo $e->getMessage();
-		}
-	 ?>
-				<div class="span4">
-					<ul class="nav nav-list">
-						<li class="nav-header">Quick Access</li>
-						<li class="active"><a href="create_subject.php">Create Subjects</a></li>
-						<li><a href="#">Account details</a></li>
-						<li><a href="#">My account</a></li>
-						<li class="divider"></li>
-						<li><a href="#">About Us</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-		
-		<!-- Footer -->
-		<?php
-			include '../footer.php';
-		?>
-
-		<script src="http://code.jquery.com/jquery-1.9.0.min.js"></script>
-		<script src="../../js/bootstrap.js"></script>
-	</body>
-</html>
--- php/subjects/edit_subject.php
+++ php/subjects/edit_subject.php
@@ -3,47 +3,26 @@
 	include '../getConnection.php';
 	require '../check_logged_in.php';
 
-	//to create subject you need SubjectID, SubjectCode(3char 5 num), SubjectName, UserID, Dateupdated
-
-	//this query selects teachers & coordinators, and displays only user id and names.
-	//$query_teachers = mysql_query("SELECT UserID, fName, lName FROM users WHERE role = 2 OR role = 3");
-
-	//assign variables.
-	$subject_code = $_POST['subject_code'];
-	$subject_name = $_POST['subject_name'];
-	$selected_teacher = $_POST['subject_coordinator'];
-
-	//get the date in Australia and assign variable
-	date_default_timezone_set('Australia/Melbourne');
-	$date = date('Y-m-d', time());
-
-	//UPDATE SUBJECT QUERY
-	//$query_update_subject = "INSERT INTO subject VALUES(null,'".$subject_code."','".$subject_name."', '".$selected_teacher."', '".$date."')";
-
-?>
-
-<html>
-	<head>
-		<?php
-			include '../header_container.php';
-		?>
-		<title>ReviseIT - Edit Subject</title>
-	</head>
-	
-	<body>
-
-		<br /><br />
-
-		<div class="container">
-			<div class="row-fluid">
-				<?php
-					include 'edit_subject_container.php';
-				?>
-			</div>
-
-		</div>
+	if(isset($_POST['update_submit'])){
 		
-		<script src="http://code.jquery.com/jquery-1.9.0.min.js"></script>
-		<script src="../../js/bootstrap.js"></script>
-	</body>
-</html>
+		$subjectID 		= 	$_GET['ID'];
+		$subjectName 	= 	$_POST['subject_name'.$_GET['ID']];
+		$subjectCode 	= 	$_POST['subject_code'.$_GET['ID']];
+		$selectedUser 	= 	$_POST['subject_coordinator'.$_GET['ID']];
+
+		//get the date in Australia and assign variable
+		date_default_timezone_set('Australia/Melbourne');
+		$date = date('Y-m-d', time());
+
+		$query_update_string = $db->prepare("UPDATE subject SET SubjectID = :bind_SubjectID, SubjectCode = :bind_SubjectCode, SubjectName = :bind_SubjectName, UserID = :bind_UserID, Dateupdated = :bind_Date WHERE SubjectID = :bind_SubjectID");
+		$query_update_string->bindParam("bind_SubjectID", $subjectID);
+		$query_update_string->bindParam("bind_SubjectCode", $subjectCode);
+		$query_update_string->bindParam("bind_SubjectName", $subjectName);
+		$query_update_string->bindParam("bind_UserID", $selectedUser);
+		$query_update_string->bindParam("bind_Date", $date);
+		$query_update_string->execute();
+
+		header("Location: all_Subjects.php");
+
+	}
+?>
