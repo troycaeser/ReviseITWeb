eba1372e463c9b45f537f539525fab88dfe2b722
cjaybasa <cjaybasa@optusnet.com.au>
2013-05-10 17:25:46 +1000

    FIXED SUBJECTS YAAAAAAY

--- php/subtopics/new.php
+++ php/subtopics/new.php
@@ -3,14 +3,6 @@
 	require '../getConnection.php';
 	require '../check_logged_in.php';
 	
-	// get the current path
-	$current_url = $SERVER['PHP SELF'];
-
-	// process into bits
-	$path_parts = pathinfo($current_url);
-	
-	// get topic id
-	$TopicID = $_GET["ID"];
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -33,86 +25,17 @@
 	<div class="page-header">
         <h1>Add Subtopic</h1>
     </div>
-<?php
-	/* 
-	 NEW.PHP
-	 Allows user to create a new entry in the database
-	*/
-	 
-?>
 
 	<!-- subtopicname, content, date updated-->
     	<div class="row-fluid">
         	<div class="span8">
             	<div class='row-fluid'>
                     <div class='span6'><h4>Subtopic Name</h4></div>
-                    <div class='span6'><h4>Date</h4></div>
+                    <div class='span6'><h4>Brief Description</h4></div>
                     <!--<div class='span2'><h4>Coordinator</h4></div>-->
 				</div>
-                       
-<?php	 
-	 
-	 // Creates the new record form
-	 // Function created since, this form is used multiple times in this file
-	 function renderForm($Subt, $Date, $error)
-	 {
-		 // If there are any errors, display them
-		 if ($error != '')
-		 {
-			echo '<div>'.$error.'</div>';
-		 }
-	 } 
-	 
-	 // Check if the form has been submitted. If it has, start to process the form and save it to the database
-	 if (isset($_POST['submit']))
-	 { 
-		 // Get form data, making sure it is valid
-		 $SubtopicName = $_POST['SubtopicName'];
-		 $Topic_ID = $_POST['TopicID'];
-		 //$Content = $_POST['Content'];
-		 $DateUpdated = $_POST['DateUpdated'];
-		 
-		 //subtopic records
-		 //SubtopicID, SubtopicName, TopicID, Content, Downloads, DateUpdated
-		 //you need to have a way to get:
-		 //TopicID, Contents, DateUpdated (preferrably today).
 
-		 //Topic -> Subtopic.
-
-		 // Save the data to the database
-		 //$SQL = $db->prepare("INSERT INTO subtopic VALUES(NULL, :subName, :subDate, 0");
-		 //$SQL->bindParam("subName", $SubtopicName);
-		 //$SQL->bindParam("subDate", $DateUpdated);
-		 //$SQL->execute();
-		 
-		 $SQL = $db->prepare("INSERT INTO subtopic VALUES(NULL,'".$SubtopicName."','".$topic_ID."', NULL, NULL, NULL,'".$DateUpdated."')");
-		 $SQL->execute();
-		 
-	 
-		 // Check to make sure all fields are filled in
-		 if ($SubtopicName == '' || $DateUpdated == '')
-		 {
-			 // Generate error message
-			 $error = 'ERROR: Please fill in all required fields!';
-		 
-			 // If either field is blank, display the form again
-			 renderForm($SubtopicName, $DateUpdated, $error);
-	 	 }
-	 
-		 else
-		 {
-			 // Once saved, redirect back to the view page
-			 header("Location: view.php"); 
-		 }
-	 }
-	 // If the form hasn't been submitted, display the form
-	 else
-	 {
-	 	renderForm('','','','');
-	 }
-?> 
-
-    <form action="" method="post">
+    <form method="post">
        
     <table width="200" border="0">
     
@@ -124,7 +47,7 @@
         </td>
         <td>
             <label>
-                <input type="text" name="DateUpdated" />
+                <input type="text" name="briefDescription" />
             </label>
         </td>
       </tr>
@@ -162,3 +85,29 @@
                 
 </body>
 </html> 
+<?php
+	// Get form data, making sure it is valid
+	$SubtopicName = $_POST['SubtopicName'];
+	$briefDescription = $_POST['briefDescription'];
+	
+	// get topic id
+	$TopicID = $_GET["ID"];
+	
+	//get current date
+	date_default_timezone_set('Australia/Melbourne');
+	$date = date('Y-m-d', time());
+	
+	// Check if the form has been submitted. If it has, start to process the form and save it to the database
+	if (isset($_POST['submit']))
+	{ 
+		$SQL = $db->prepare("INSERT INTO subtopic VALUES(NULL, :bind_subtopicName, :bind_topicID, :bind_briefDescription, ' ', 0, :bind_date)");
+		$SQL->bindParam("bind_subtopicName", $SubtopicName);
+		$SQL->bindParam("bind_topicID", $TopicID);
+		$SQL->bindParam("bind_briefDescription", $briefDescription);
+		$SQL->bindParam("bind_date", $date);
+		$SQL->execute();
+		
+		// Once saved, redirect back to the view page
+		header("Location: view.php?ID=".$TopicID."");
+	}
+?> 
