65dc8706ffd169f6e6076162221d44fe84923e13
Kris Vega <krisv_990@hotmail.com>
2013-05-18 23:47:23 +1000

    Added some validation

--- php/topics/editTopic.php
+++ php/topics/editTopic.php
@@ -7,7 +7,6 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<link rel="stylesheet" type="text/css" href="../../assets/css/bootstrap-wysihtml5-0.0.2.css"></link>
 <link rel="stylesheet" href="../../assets/css/version1.css">
 <link rel="stylesheet" href="../../assets/css/bootstro.css">
 <link rel="stylesheet" href="../../assets/css/bootstrap-responsive.css">
@@ -27,24 +26,30 @@
 		{
 			date_default_timezone_set('Australia/Melbourne');
 			$date = date('Y-m-d', time());
-		
-			$topicName = htmlentities($_POST['topicName']);	
-			$subCode = htmlentities($_POST['SubjCode']);
+			
+			if(empty($topicName))
+			{
+				die("Topic name must be alphabetic");
+			}
+			else
+			{
+				$topicName = htmlentities($_POST['topicName']);	
+				$subCode = htmlentities($_POST['SubjCode']);
+					
+				$stmt = $db->prepare("UPDATE topic SET TopicName = :topicName, SubjectCode = :subCode, dateupdated = :date WHERE TopicID = :top_ID");
+				$stmt->bindParam("top_ID", $topic_ID);
+				$stmt->bindParam("topicName", $topicName);
+				$stmt->bindParam("subCode", $subCode);
+				$stmt->bindParam("date", $date);
+				$stmt->execute();
+				 
+				$query = $db->prepare("SELECT SubjectID FROM topic WHERE TopicID = :top_ID");
+				$query->bindParam("top_ID", $topic_ID);
+				$query->execute();
+				$stuff = $query->fetchColumn();
 				
-			$stmt = $db->prepare("UPDATE topic SET TopicName = :topicName, SubjectCode = :subCode, dateupdated = :date WHERE TopicID = :top_ID");
-			$stmt->bindParam("top_ID", $topic_ID);
-			$stmt->bindParam("topicName", $topicName);
-			$stmt->bindParam("subCode", $subCode);
-			$stmt->bindParam("date", $date);
-			$stmt->execute();
-			 
-			
-			$query = $db->prepare("SELECT SubjectID FROM topic WHERE TopicID = :top_ID");
-			$query->bindParam("top_ID", $topic_ID);
-			$query->execute();
-			$stuff = $query->fetchColumn();
-			
-			header("Location: viewTopic.php?ID=".$stuff);
+				header("Location: viewTopic.php?ID=".$stuff);
+			}
 		}
 		catch(PDOException $e)
 		{
