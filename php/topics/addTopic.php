df5009eff9e07fce1d06192ad67a523d7e8287e5
Kris Vega <krisv_990@hotmail.com>
2013-05-20 22:11:29 +1000

    Added validation, only enables you to enter alphabetic characters

--- php/topics/addTopic.php
+++ php/topics/addTopic.php
@@ -1,3 +1,7 @@
+<link rel="stylesheet" href="../../assets/css/version1.css">
+<link rel="stylesheet" href="../../assets/css/bootstro.css">
+<link rel="stylesheet" href="../../assets/css/bootstrap-responsive.css">
+
 <?php
 	include '../getConnection.php';
 	
@@ -5,36 +9,44 @@
 	{
 		try
 		{
-			if(empty($topicName))
+			$topicName = $_POST["strtopicName"];
+			
+			if(!empty($topicName))
 			{
-				die("Topic Name must be alphabetic");
+				if(!preg_match("/[A-Za-z]/", $topicName))	
+				{
+					echo "<div class='alert alert-error' align='center'>Topic name must be alphabetic</div>";
+				}
+				else
+				{
+					//This variable is used to obtain the SubjectID from the subject code
+					$subjectCode = $_POST["subject_code"];
+					
+					date_default_timezone_set('Australia/Melbourne');
+					$date = date('Y-m-d', time());
+					
+					$subIDsql = "SELECT SubjectCode FROM subject WHERE SubjectID = :subject_id";
+					$sql = $db->prepare($subIDsql);
+					$sql->bindParam("subject_id", $subjectCode);
+					$sql->execute();
+					
+					$row = $sql->fetchColumn();
+				
+					//This sql query is used to insert the respective variable into the topic table
+					$resultSQL = $db->prepare("INSERT INTO topic VALUES(null, :bind_TopicName, :bind_SubID, :bind_SubCode ,  0, :bind_date)");
+					$resultSQL->bindParam("bind_TopicName", $topicName);
+					$resultSQL->bindParam("bind_SubID", $subjectCode);
+					$resultSQL->bindParam("bind_SubCode", $row);
+					$resultSQL->bindParam("bind_date", $date);
+					$resultSQL->execute();
+					
+					header("location: viewTopic.php?ID=".$subjectCode);	
+				}
 			}
 			else
 			{
-				$topicName = $_POST["strtopicName"];
-				
-				//This variable is used to obtain the SubjectID from the subject code
-				$subjectCode = $_POST["subject_code"];
-				
-				date_default_timezone_set('Australia/Melbourne');
-				$date = date('Y-m-d', time());
-				
-				$subIDsql = "SELECT SubjectCode FROM subject WHERE SubjectID = :subject_id";
-				$sql = $db->prepare($subIDsql);
-				$sql->bindParam("subject_id", $subjectCode);
-				$sql->execute();
-				
-				$row = $sql->fetchColumn();
-			
-				//This sql query is used to insert the respective variable into the topic table
-				$resultSQL = $db->prepare("INSERT INTO topic VALUES(null, :bind_TopicName, :bind_SubID, :bind_SubCode ,  0, :bind_date)");
-				$resultSQL->bindParam("bind_TopicName", $topicName);
-				$resultSQL->bindParam("bind_SubID", $subjectCode);
-				$resultSQL->bindParam("bind_SubCode", $row);
-				$resultSQL->bindParam("bind_date", $date);
-				$resultSQL->execute();
-				
-				header("location: viewTopic.php?ID=".$subjectCode);
+				echo "<div class='alert alert-error' align='center'>Topic name must not be empty</div>";
+				//header("Location: newTopic.php");
 			}
 			
 		}
