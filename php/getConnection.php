b0a14a5525b73e6bfb1ce31ea5e9c031bd32a94b
troycaeser <troycaeser@hotmail.com>
2013-06-07 17:17:45 +1000

    changed connection

--- php/getConnection.php
+++ php/getConnection.php
@@ -19,23 +19,23 @@
     */
     
 
-    $config['db'] = array(
-        'host'          =>'localhost',
-        'username'      =>'root',
-        'password'      =>'root',
-        'dbname'        =>'reviseit'
-    );
-
-
-    $db = new PDO('mysql:host='.$config['db']['host'].';dbname='.$config['db']['dbname'], $config['db']['username'], $config['db']['password']);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     // $config['db'] = array(
-    //     'host'          =>'reviseithg.db.11048397.hostedresource.com',
-    //     'username'      =>'reviseithg',
-    //     'password'      =>'ReviseIT!2013',
-    //     'dbname'        =>'reviseithg'
+    //     'host'          =>'localhost',
+    //     'username'      =>'root',
+    //     'password'      =>'root',
+    //     'dbname'        =>'reviseit'
     // );
+
+
+    // $db = new PDO('mysql:host='.$config['db']['host'].';dbname='.$config['db']['dbname'], $config['db']['username'], $config['db']['password']);
+    // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $config['db'] = array(
+        'host'          =>'reviseithg.db.11048397.hostedresource.com',
+        'username'      =>'reviseithg',
+        'password'      =>'ReviseIT!2013',
+        'dbname'        =>'reviseithg'
+    );
 
 
     // $db = new PDO('mysql:host='.$config['db']['host'].';dbname='.$config['db']['dbname'], $config['db']['username'], $config['db']['password']);
